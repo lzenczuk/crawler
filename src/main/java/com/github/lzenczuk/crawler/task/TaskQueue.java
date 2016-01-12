f0584aa5de29218b58a668334927d6e9f59ff8b3
Lucjan Zenczuk <lzenczuk@xanadu.ie>
2016-01-12 17:20:43 +0000

    Add multiple threads to TaskAQueue.

--- src/main/java/com/github/lzenczuk/crawler/task/TaskQueue.java
+++ src/main/java/com/github/lzenczuk/crawler/task/TaskQueue.java
@@ -5,8 +5,7 @@ import com.github.lzenczuk.crawler.script.Result;
 import com.github.lzenczuk.crawler.script.ScriptRunner;
 
 import java.util.Optional;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.*;
 
 /**
  * @author lzenczuk 12/01/2016
@@ -23,13 +22,18 @@ public class TaskQueue {
         this.browser = browser;
         this.runner = runner;
 
+        final ExecutorService executorService = Executors.newFixedThreadPool(5);
+
         final Thread thread = new Thread(() -> {
             while (true) {
                 try {
-                    final Task task = taskQueue.take();
-                    final Optional<Result> resultOptional = runner.run(task.getScript(), task.getParams(), browser);
 
-                    System.out.println(resultOptional.map(result -> result.toString()).orElse("No result"));
+                    final Task task = taskQueue.take();
+
+                    executorService.submit(() -> {
+                        final Optional<Result> resultOptional = runner.run(task.getScript(), task.getParams(), browser);
+                        System.out.println(resultOptional.map(result -> result.toString()).orElse("No result"));
+                    });
 
                 } catch (InterruptedException e) {
                     System.out.println("Exception: "+e.getMessage());
